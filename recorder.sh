a52e3a1966346ccbb8cc799c4d435545c62979f2
SPDUK <spdevuk@gmail.com>
2020-03-29 23:00:12 +0100

    chore: lint

--- recorder.sh
+++ recorder.sh
@@ -2,16 +2,16 @@
 
 if pidof ffmpeg
   then
-  killall ffmpeg
-  notify-send 'Stopped Recording!' --icon=dialog-information
+    killall ffmpeg
+    notify-send 'Stopped Recording!' --icon=dialog-information
   else 
-  slop=$(slop -f "%x %y %w %h %g %i")
+    slop=$(slop -f "%x %y %w %h %g %i")
 
-  read -r X Y W H G ID < <(echo $slop)
+    read -r X Y W H G ID < <(echo $slop)
 
-  time=$(date +%F%T)
+    time=$(date +%F%T)
 
-  notify-send 'Started Recording!' --icon=dialog-information
+    notify-send 'Started Recording!' --icon=dialog-information
 
-  ffmpeg -nostdin -f x11grab -s "$W"x"$H" -i :1.0+$X,$Y -f alsa -i pulse ~/Videos/recording-$time.mp4 
+    ffmpeg -nostdin -f x11grab -s "$W"x"$H" -i :1.0+$X,$Y -f alsa -i pulse ~/Videos/recording-$time.mp4 
 fi
