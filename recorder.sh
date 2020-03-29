1f3d6dd3e6477cd47d2c432a3d0d6fc0a0098306
SPDUK <spdevuk@gmail.com>
2020-03-29 22:59:51 +0100

    feat: use bash script instead of ruby + make it toggle on / off

---
+++ recorder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if pidof ffmpeg
+  then
+  killall ffmpeg
+  notify-send 'Stopped Recording!' --icon=dialog-information
+  else 
+  slop=$(slop -f "%x %y %w %h %g %i")
+
+  read -r X Y W H G ID < <(echo $slop)
+
+  time=$(date +%F%T)
+
+  notify-send 'Started Recording!' --icon=dialog-information
+
+  ffmpeg -nostdin -f x11grab -s "$W"x"$H" -i :1.0+$X,$Y -f alsa -i pulse ~/Videos/recording-$time.mp4 
+fi
