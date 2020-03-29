2605b005b614689f8179dbaf70e1188236bdd2fe
SPDUK <spdevuk@gmail.com>
2020-03-30 00:27:08 +0100

    feat: allow escape to be pressed to not record

--- recorder.sh
+++ recorder.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-
 if pidof ffmpeg
   then
     killall ffmpeg
+
     notify-send 'Stopped Recording!' --icon=dialog-information
   else 
-    slop=$(slop -f "%x %y %w %h %g %i")
+    slop=$(slop -f "%x %y %w %h")
 
-    read -r X Y W H G ID < <(echo $slop)
+    read -r X Y W H < <(echo $slop)
 
     time=$(date +%F%T)
 
-    notify-send 'Started Recording!' --icon=dialog-information
 
-    ffmpeg -nostdin -f x11grab -s "$W"x"$H" -i :1.0+$X,$Y -f alsa -i pulse ~/Videos/recording-$time.mp4 
+    # only start recording if we give a width (e.g we press escape to get out of slop - don't record)
+    width=${#W}
+
+    if [ $width -gt 0 ];
+     then
+      notify-send 'Started Recording!' --icon=dialog-information
+      # records without audio for audio add: -f alsa -i pulse 
+      ffmpeg \
+      -nostdin -threads 4 -thread_queue_size 512 -framerate 60 \
+      -f x11grab -s "$W"x"$H" \
+      -i $DISPLAY.0+$X,$Y ~/Videos/recording-$time.mp4 
+    fi
 fi
